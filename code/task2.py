6c03e10e0f307a360eb3078d44ed9e1f6d135a75
skokov <sk1056@york.ac.uk>
2017-04-26 22:35:46 +0100

    Implemented most of EM. Logliklihood remaining

--- code/task2.py
+++ code/task2.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 import sys
+import math
 
 
 def read_file(input_file):
@@ -26,14 +27,22 @@ def read_file(input_file):
 
 
 class HMM:
-    initial = []
-    transition = []
-    emission = []
+    def __init__(self, rand_init=True, number_of_hidden_states=16, number_of_visible_states=3):
+        self.hidden_states = number_of_hidden_states
+        self.visible_states = number_of_visible_states
 
-    def __init__(self):
-        self.initial = [random.random() for i in range(16)]
-        self.transition = [[random.random() for i in range(16)] for j in range(16)]
-        self.emission = [[random.random() for i in range(16)] for j in range(3)]
+        if rand_init:
+            self.initial = [random.random() for i in range(self.hidden_states)]
+            self.transition = [[random.random() for i in range(self.hidden_states)] for j in range(self.hidden_states)]
+            self.emission = [[random.random() for i in range(self.hidden_states)] for j in range(self.visible_states)]
+        else:
+            self.initial = [1.0 / self.hidden_states for i in range(self.hidden_states)]
+            self.transition = [[1.0 / self.hidden_states for i in range(self.hidden_states)] for j in
+                               range(self.hidden_states)]
+            self.emission = [[1.0 / self.visible_states for i in range(self.hidden_states)] for j in
+                             range(self.visible_states)]
+
+        self.likelihood = 100
 
     def _prob_repr(self, prob):
         return str(round(float(prob), 4))
@@ -59,188 +68,253 @@ class HMM:
             return True
 
     def __str__(self):
-        rep = "Initial\n"
-        rep += 'ht\\ht-1'
+        DASHES = 135
+        rep = ''
+        rep += '-' * DASHES + '\n'
+        rep += "Initial\n"
+        rep += '-' * DASHES + '\n'
+        rep += ''
+        for i in range(self.hidden_states):
+            rep += '(' + str(i / 4) + ',' + str(i % 4) + ')\t'
+        rep += '\n\n'
 
         for i in self.initial:
             rep += self._prob_repr(i) + '\t'
 
-        rep += '\nTransition\n'
+        rep += '\n' + '-' * DASHES + '\n'
+        rep += 'Transition\n'
+        rep += '-' * DASHES + '\n'
+
+        rep += ''
+        for i in range(self.hidden_states):
+            rep += '\t(' + str(i / 4) + ',' + str(i % 4) + ')'
+        rep += '\n\n'
+
+        count = 0
         for i in self.transition:
+            rep += '(' + str(count / 4) + ',' + str(count % 4) + ')\t'
+            count += 1
             for j in i:
                 rep += self._prob_repr(j) + '\t'
             rep += '\n'
 
-        rep += '\nEmission\n'
+        rep += '-' * DASHES + '\n'
+        rep += 'Emission\n'
+        rep += '-' * DASHES + '\n'
+
+        for i in range(self.hidden_states):
+            rep += '\t(' + str(i / 4) + ',' + str(i % 4) + ')'
+        rep += '\n\n'
+
+        count = -1
         for i in self.emission:
+            rep += '(' + str(count) + ')\t'
+            count += 1
             for j in i:
                 rep += self._prob_repr(j) + '\t'
             rep += '\n'
+        rep += '-' * DASHES + '\n'
         return rep
 
+    def getLikelihood(self, alpha):
+        E = len(alpha)  # Number of episodes
+        N = self.hidden_states  # Number of hidden states
+        likelihood = 0
+
+        for episode in alpha:
+            T = len(episode)  # Length of episode
+            summation = 0
+
+            for state in range(N):
+                summation += math.exp(episode[T-1][state])
+
+            likelihood += math.log(summation/ E)
+        return likelihood
+
     def forward(self, episodes):
-        N = len(self.initial)  # Number of states
+        N = self.hidden_states  # Number of hidden states
         alpha = []
+        loglik = 0
 
         for episode in episodes:
             T = len(episode)  # Length of episode
             F = [[0 for i in range(N)] for j in range(T)]
 
+            # Calculate alpha(h1)
             for state in range(N):
-                F[0][state] = self.emission[episode[0]][state] * self.initial[state]  # Calculate alpha(h1)
+                F[0][state] = math.log(self.emission[episode[0]][state] * self.initial[state])
 
+            # Calculate alpha(ht)
             for t in range(1, T):
                 for state in range(N):
                     summation = 0
                     for lastState in range(N):
-                        summation += self.transition[state][lastState] * F[t - 1][lastState]
-                    F[t][state] = self.emission[episode[t]][state] * summation  # Calculate alpha(ht)
+                        summation += self.transition[state][lastState] * math.exp(F[t - 1][lastState])
+
+                    F[t][state] = math.log(self.emission[episode[t]][state] * summation)
+
             alpha.append(F)
 
         return alpha
 
     def backward(self, episodes):
-        N = len(self.initial)  # Number of states
+        N = self.hidden_states  # Number of states
         beta = []
 
         for episode in episodes:
             T = len(episode)  # Length of episode
             F = [[0 for i in range(N)] for j in range(T)]
 
+            # Calculate beta(h(T))
             for state in range(N):
                 F[-1][state] = 1
 
-            for t in reversed([i for i in range(1, T)]):
-                for lastState in range(N):
+            # Calculate beta(h(t-1))
+            for t in reversed(range(1, T)):
+                for previousState in range(N):
                     summation = 0
                     for state in range(N):
-                        summation += self.emission[episode[t]][state] * self.transition[state][lastState] * \
-                                     F[t][state]
-
-                    F[t - 1][lastState] = summation
-
+                        summation += self.emission[episode[t]][state] * self.transition[state][previousState] * \
+                                     math.exp(F[t][state])
+                    F[t - 1][previousState] = math.log(summation)
             beta.append(F)
         return beta
 
-    # p(h|v)
     def getGamma(self, alpha, beta):
-        E = len(alpha)
-        N = len(self.initial)
+        E = len(alpha)  # Number of episodes
+        N = self.hidden_states  # Number of hidden states
         gamma = []
 
-        for episode in range(E):
-            T = len(alpha[episode])  # Length of episode
+        for ep_num in range(E):
+            T = len(alpha[ep_num])  # Length of episode
             F = [[0 for i in range(N)] for j in range(T)]
 
             for t in range(T):
                 summation = 0
+
+                # Calculate normalization factor
                 for state in range(N):
-                    summation += alpha[episode][t][state] * beta[episode][t][state]
+                    summation += alpha[ep_num][t][state] * beta[ep_num][t][state]
+
+                # Calculate p(h(t)|v(1:T))
                 for state in range(N):
                     try:
-                        F[t][state] = alpha[episode][t][state] * beta[episode][t][state] / summation
+                        F[t][state] = alpha[ep_num][t][state] * beta[ep_num][t][state] / summation
                     except ZeroDivisionError:
                         F[t][state] = 0
 
             gamma.append(F)
-
         return gamma
 
-    # p(ht,ht+1|v)
     def getXi(self, alpha, beta, episodes):
-        E = len(episodes)
-        N = len(self.initial)
+        E = len(episodes)  # Number of episodes
+        N = self.hidden_states  # Number of hidden states
         xi = []
 
-        for episode in range(E):
-            T = len(episodes[episode])  # Length of episode
+        for ep_num in range(E):
+            T = len(episodes[ep_num])  # Length of episode
             F = [[[0 for i in range(N)] for j in range(N)] for k in range(T - 1)]
 
             for t in range(T - 1):
                 summation = 0
+
+                # Calculate normalization factor
                 for state in range(N):
                     for nextState in range(N):
-                        summation += alpha[episode][t][state] * self.emission[episodes[episode][t + 1]][nextState] * \
-                                     self.transition[nextState][state] * beta[episode][t + 1][nextState]
+                        summation += alpha[ep_num][t][state] * self.emission[episodes[ep_num][t + 1]][nextState] * \
+                                     self.transition[nextState][state] * beta[ep_num][t + 1][nextState]
+
+                # Calculate p(h(t),h(t+1)|v(1:T))
                 for state in range(N):
                     for nextState in range(N):
                         try:
-                            F[t][state][nextState] = alpha[episode][t][state] * self.emission[episodes[episode][t + 1]][
-                                nextState] * self.transition[nextState][state] * beta[episode][t + 1][
-                                                         nextState] / summation
+                            F[t][state][nextState] = alpha[ep_num][t][state] * self.transition[nextState][state] * \
+                                                     self.emission[episodes[ep_num][t + 1]][nextState] * \
+                                                     beta[ep_num][t + 1][nextState] / summation
                         except ZeroDivisionError:
                             F[t][state][nextState] = 0
-            # printF(F)
             xi.append(F)
         return xi
 
     def updateInitial(self, gamma):
         E = len(gamma)  # Number of episodes
-        S = len(self.initial)
-        self.initial = [0 for i in range(16)]
+        N = self.hidden_states  # Number of hidden states
+        self.initial = [0 for i in range(N)]
 
         for episode in gamma:
-            for state in range(S):
+            for state in range(N):
                 self.initial[state] += episode[0][state] / E
 
     def updateTransition(self, gamma, xi):
         E = len(gamma)  # Number of episodes
-        S = len(self.initial)
-        self.transition = [[0 for i in range(16)] for j in range(16)]
+        N = self.hidden_states  # Number of hidden states
+        self.transition = [[0 for i in range(N)] for j in range(N)]
 
         for ep_num in range(E):
-            T = len(xi[ep_num])  # Timesteps in episode
-
-            summations = [0 for i in range(16)]
-
-            for t in range(T):
-                for state1 in range(S):
-                    summations[state1] += gamma[ep_num][t][state1]
+            T = len(xi[ep_num])  # Length of episode
+            summations = [0 for i in range(N)]
 
             for t in range(T):
-                for state1 in range(S):
-                    for state2 in range(S):
+                for state in range(N):
+                    summations[state] += gamma[ep_num][t][state]
+
+            for t in range(T):
+                for state in range(N):
+                    for nextState in range(N):
                         try:
-                            self.transition[state2][state1] += (
-                                                                   (xi[ep_num][t][state1][state2] / summations[
-                                                                       state1])) / E
+                            self.transition[nextState][state] += (xi[ep_num][t][state][nextState] / summations[
+                                state]) / E
                         except ZeroDivisionError:
-                            self.transition[state2][state1] += 0
-        return
+                            self.transition[nextState][state] += 0
 
     def updateEmission(self, gamma, episodes):
         E = len(gamma)  # Number of episodes
-        S = len(self.initial)  # Number of episodes
-        self.emission = [[0 for i in range(16)] for j in range(3)]
+        N = self.hidden_states  # Number of hidden states
+        self.emission = [[0 for i in range(N)] for j in range(3)]
 
         for ep_num in range(E):
-            T = len(episodes[ep_num])  # Timesteps in episode
-            statesums = [0 for i in range(S)]
-            newemmission = [[0 for i in range(16)] for j in range(3)]
+            T = len(episodes[ep_num])  # Length of episode
+            statesums = [0 for i in range(N)]
+            newemmission = [[0 for i in range(N)] for j in range(3)]
 
             for t in range(T):
-                for state in range(S):
+                for state in range(N):
                     statesums[state] += gamma[ep_num][t][state]
 
             for t in range(T):
-                for state in range(S):
+                for state in range(N):
                     try:
                         newemmission[episodes[ep_num][t]][state] += gamma[ep_num][t][state] / statesums[state]
                     except ZeroDivisionError:
                         newemmission[episodes[ep_num][t]][state] = 0
+
             for i in range(len(newemmission)):
                 for j in range(len(newemmission[i])):
                     self.emission[i][j] += newemmission[i][j] / E
-        return
 
-    def baum_welch(self, episodes):
-        alpha = self.forward(episodes)
-        beta = self.backward(episodes)
-        gamma = self.getGamma(alpha, beta)
-        xi = self.getXi(alpha, beta, episodes)
-        self.updateInitial(gamma)
-        self.updateEmission(gamma, episodes)
-        self.updateTransition(gamma, xi)
+    def baum_welch(self, episodes, verbouse=False):
+        iteration = 0
+        while True:
+            iteration += 1
+            if verbouse:
+                print self.__str__()
+                print 'Log-likelihood', self.likelihood, '\n'
+                print 'Iteration:', iteration
+            alpha = self.forward(episodes)
+            beta = self.backward(episodes)
+            gamma = self.getGamma(alpha, beta)
+            xi = self.getXi(alpha, beta, episodes)
+            self.updateInitial(gamma)
+            self.updateEmission(gamma, episodes)
+            self.updateTransition(gamma, xi)
+            newLikelihood = self.getLikelihood(alpha)
+            print 'Diferrence', self.likelihood - newLikelihood
+            if abs(self.likelihood - newLikelihood) < 0.01:
+                break
+
+            self.likelihood = newLikelihood
+
+        return iteration
 
 
 def printF(F):
@@ -249,29 +323,22 @@ def printF(F):
         for j in i:
             print str(j) + '\t',
         print
-    return
 
-    def beta(self):
-        pass
+    return
 
 
 def task2(input_file):
-    print "task2 " + input_file
+    print 'Task2'
+    print 'Input fille is:', input_file
     episodes = read_file(input_file)
-    hmm = HMM()
+    hmm = HMM(False)
+    hmm.baum_welch(episodes,True)
     print hmm
-    print hmm.probs_sum_to_1()
-    for i in range(1000):
-        hmm.baum_welch(episodes)
-        print hmm
-        print hmm.probs_sum_to_1()
-        print
-    return
+    print 'Log-likelihood:', hmm.likelihood
 
 
 if __name__ == '__main__':
     input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'task2.dat')
-    print 'Input file is: ' + input_file
     if not os.path.exists(input_file):
         print("Input file doesn't exist!")
     else:
--- code/task3.py
+++ code/task3.py
@@ -1,14 +1,29 @@
 import os
+from task2 import HMM
+from task2 import read_file
 
 
 def task3(input_file):
-    print "task3 " + input_file
+    print 'Task3'
+    print 'Input fille is:', input_file
+    episodes = read_file(input_file)
+    print episodes
     return
+    newlist = []
+    for i in episodes:
+         newlist.extend(i[1:])
+    print newlist
+
+    hmm = HMM(False)
+    for i in range(1):
+        hmm.baum_welch([newlist], True)
+        print hmm
+        print 'Log-likelihood:', hmm.likelihood
+        print
 
 
 if __name__ == '__main__':
     input_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'task2.dat')
-    print 'Input file is: ' + input_file
     if not os.path.exists(input_file):
         print("Input file doesn't exist!")
     else:
